8b0c69a02c24e2687d0ce4cbee0f105dcced8dfc
DassVeryGood <idass303@gmail.com>
2025-05-26 10:49:18 -0500

    Added a button that calls a random number generator. general functionality

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 console.log("Hello World");
 document.getElementById("demo").innerHTML = "Hello World!";
+function generate()
+{
+    let num = Math.floor(Math.random() * 5);
+    document.getElementById("demo").innerHTML = num;
+}
